458b7a2504f024a9344d9e94dc412aa36a7200e4
madong <viaimperiale@gmail.com>
2023-08-20 12:56:03 +0200

    fix(style.css): add bg-default

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,11 @@
-export type Color = "default" | "danger" | "warning" | "success";
+export type Color = "default" | "danger" | "warning" | "success" | "info";
 
 export type HoverColor =
   | "default-hover"
   | "danger-hover"
   | "warning-hover"
-  | "success-hover";
+  | "success-hover"
+  | "info-hover";
 
 export const sizes = ["xs", "sm", "md", "lg", "xl"] as const;
 export type Size = (typeof sizes)[number];
