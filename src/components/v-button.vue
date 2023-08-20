a0d265feb45d48b0ea3fc556330406646ffe828d
madong <viaimperiale@gmail.com>
2023-08-20 12:33:39 +0200

    refactor(v-btn): rename to v-button

---
+++ src/components/v-button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    :class="[colorMap[color || 'default'], sizeMap[size || 'md']]"
+    class="rounded-md font-semibold text-white shadow-md"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { Color, Size } from "../types.ts";
+
+defineProps<{
+  disabled?: boolean;
+  size?: Size;
+  color?: Color;
+}>();
+
+const colorMap: Record<Color, string> = {
+  default: "bg-default hover:bg-default-hover",
+  danger: "bg-danger hover:bg-danger-hover",
+  warning: "bg-warning hover:bg-warning-hover",
+  success: "bg-success hover:bg-success-hover",
+};
+
+const sizeMap: Record<Size, string> = {
+  xs: "text-xs px-1 py-0.5",
+  sm: "text-sm px-2 py-1",
+  md: "px-3 py-1.5",
+  lg: "text-lg px-4 py-2",
+  xl: "text-xl px-5 py-2.5",
+};
+</script>
+
+<style scoped></style>
