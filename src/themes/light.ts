4925549179b8ea9f8f2c3a26c1d933fbaf5725b3
madong <viaimperiale@gmail.com>
2023-08-20 13:03:38 +0200

    fix(themes/light): tunes hover colors

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -3,15 +3,15 @@ import { Color, HoverColor } from "../types.ts";
 export const lightColors: Record<Color, string> = {
   default: "#0066ff",
   danger: "#E73D23",
-  warning: "#F9DC5C",
+  warning: "#F9D94E",
   success: "#2EB800",
   info: "#0066ff",
 } as const;
 
 export const lightHoverColors: Record<HoverColor, string> = {
-  "default-hover": "#005AE0",
-  "danger-hover": "#CA2E16",
+  "default-hover": "#0052CC",
+  "danger-hover": "#B82A14",
   "success-hover": "#248F00",
-  "warning-hover": "#F8D53A",
-  "info-hover": "#005AE0",
+  "warning-hover": "#ECC209",
+  "info-hover": "#0052CC",
 } as const;
