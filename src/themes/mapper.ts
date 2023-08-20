bc01031f2e736177dadf59674458067947390c93
madong <viaimperiale@gmail.com>
2023-08-21 00:29:16 +0200

    fix(mapper/Input): input 1px higher than button

--- src/themes/mapper.ts
+++ src/themes/mapper.ts
@@ -36,7 +36,7 @@ export const Input: Mapper = {
   Size: {
     xs: "text-xs px-1 py-[1px] border-[1px] rounded focus:ring-[0.5px]",
     sm: "text-sm px-2 py-[3px] border-[1px] rounded-md focus:ring-[0.5px]",
-    md: "px-3 py-[5px] border-[1.5px] rounded-lg focus:ring-1",
+    md: "px-3 py-[4.5px] border-[1.5px] rounded-lg focus:ring-1",
     lg: "text-lg px-4 py-[7px] border-2 rounded-xl focus:ring-1",
     xl: "text-xl px-5 py-[8px] border-2 rounded-2xl focus:ring-1",
   },
